99cb43cad343f24cabb0cb1afd1c961db9e05576
Andrey Belousow <groundhog448@gmail.com>
2017-04-12 18:45:17 +0400

    Запрос режима на старте

--- anime/anime.go
+++ anime/anime.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"anime/controllers"
 
 	"strconv"
@@ -10,43 +12,76 @@ import (
 	"net/http"
 
 	"github.com/go-martini/martini"
+
+	"bufio"
 )
 
 func main() {
 
-	//Джисус, таск по серверу:
-	//в ответ на запрос приходит код, обозначающий,
-	//может ли работать приложение. логика тупейшая:
-	//если vremya < nachalo_festa тогда вернуть 1488 иначе вернуть 200
-
 	m := martini.Classic()
-	
-	canWork bool := false;
+
+	canWork := false
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("ALLOW ACESS? [y/n]")
+
+	flag := true
+	for flag {
+		char, _, err := reader.ReadRune()
+		if err != nil {
+		}
+		switch char {
+		case 'y':
+			fmt.Println("ACESS ALLOWED")
+			canWork = true
+			flag = false
+			break
+		case 'n':
+			fmt.Println("ACESS DENIED")
+			canWork = false
+			flag = false
+			break
+		}
+	}
 
 	//Обращение к табличке с результатами
 	m.Get("/table", controllers.ShowUsersTable)
 
-	//Увеличение счетчика у пользователя, стоит заменить на POST
-	m.Get("/update", func(req *http.Request) {
-		requrl := req.URL.Query()
-		userid := requrl.Get("id")
-		count, err := strconv.Atoi(requrl.Get("count"))
-		if err != nil {
-			fmt.Println(err)
+	//Увеличение счетчика у пользователя
+	m.Get("/update", func(res http.ResponseWriter, req *http.Request) {
+		if canWork {
+			requrl := req.URL.Query()
+			userid := requrl.Get("id")
+			count, err := strconv.Atoi(requrl.Get("count"))
+			if err != nil {
+				fmt.Println(err)
+			}
+			controllers.Update(userid, count)
+			res.WriteHeader(200)
+			fmt.Println("__________________________________________")
+			fmt.Println("UPDATED id:", userid, " count:", count)
+			fmt.Println("__________________________________________")
 		}
-		controllers.Update(userid, count)
+		res.WriteHeader(1488)
 		fmt.Println("__________________________________________")
-		fmt.Println("UPDATED id:", userid, " count:", count)
+		fmt.Println("ACESS DENIED")
 		fmt.Println("__________________________________________")
 	})
 
 	//Удаление пользователя
-	m.Get("/delete", func(req *http.Request) {
-		requrl := req.URL.Query()
-		userid := requrl.Get("id")
-		controllers.Delete(userid)
+	m.Get("/delete", func(res http.ResponseWriter, req *http.Request) {
+		if canWork {
+			requrl := req.URL.Query()
+			userid := requrl.Get("id")
+			controllers.Delete(userid)
+			res.WriteHeader(200)
+			fmt.Println("__________________________________________")
+			fmt.Println("DELETED id:", userid)
+			fmt.Println("__________________________________________")
+		}
+		res.WriteHeader(1488)
 		fmt.Println("__________________________________________")
-		fmt.Println("DELETED id:", userid)
+		fmt.Println("ACESS DENIED")
 		fmt.Println("__________________________________________")
 	})
 
