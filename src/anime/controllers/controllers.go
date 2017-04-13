9eb1d2d73a70a999aa1f5de311f627d2205cc489
AndreyBelousow <groundhog448@gmail.com>
2017-04-13 23:29:39 +0400

    Выключатель, убран кек

--- src/anime/controllers/controllers.go
+++ src/anime/controllers/controllers.go
@@ -1,23 +1,23 @@
 package controllers
 
-//"anime/database"
+"anime/database"
 
 //"anime/view"
 
 //Увеличение счетчика у пользователя
 func Update(userid string, count int) string {
-	//database.Update(userid, count)
+	database.Update(userid, count)
 	return "updated " + userid
 }
 
 //Удаление пользователя
 func Delete(userid string) string {
-	//database.Delete(userid)
+	database.Delete(userid)
 	return "deleted " + userid
 }
 
 //Показать табличку с результатами
 func ShowUsersTable() string {
-	//return database.Kek()
+	return database.Kek()
 	return "here is table"
 }
